ec20aae338c02d622537294b60b7852260e6701f
hyyc554 <1415940604@qq.com>
2020-02-09 19:46:56 +0800

    2.0

--- hello.go
+++ hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+const HelloHead = "Hello, "
+
+func Hello(name string) string {
+	return HelloHead + name
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("WuHan"))
 }
