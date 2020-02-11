2dfe7660c40850df97a67ebc8a4a8cbce0d05fb7
hyyc554 <1415940604@qq.com>
2020-02-11 17:52:41 +0800

    迭代

---
+++ interation/repeat_test.go
@@ -0,0 +1,24 @@
+package interation
+
+import "testing"
+
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < 5; i++ {
+		repeated += character
+	}
+	return repeated
+}
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected '%q' but got '%q'", expected, repeated)
+	}
+}
