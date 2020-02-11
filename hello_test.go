db767b1d40c749a1d68d6fe13b2529d06a0af78c
hyyc554 <1415940604@qq.com>
2020-02-11 15:06:48 +0800

    空值测试

--- hello_test.go
+++ hello_test.go
@@ -2,20 +2,35 @@ package main
 
 import "testing"
 
+/*
+编写一个测试
+
+让编译通过
+
+运行测试，查看失败原因并检查错误消息是很有意义的
+
+编写足够的代码以使测试通过
+
+重构
+
+*/
+
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper() // t.Helper() 需要告诉测试套件这个方法是辅助函数（helper）
 		if got != want {
 			t.Errorf("got '%q' want '%q'", got, want)
 		}
+	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got '%q' want '%q'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
